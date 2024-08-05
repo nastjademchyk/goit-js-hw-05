0bac5d5ea9e31e5d3fe9bdb1975fa8936aea1707
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-08-05 12:53:43 +0200

    resolved 4 tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,36 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price+deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+const getUsersWithFriend = (users, friendName) => users.filter((user)=> user.friends.includes(friendName)); 
+
+const allUsers = [
+  {
+    name: 'Moore Hensley',
+    friends: ['Sharron Pace'],
+  },
+  {
+    name: 'Sharlene Bush',
+    friends: ['Briana Decker', 'Sharron Pace'],
+  },
+  {
+    name: 'Ross Vazquez',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+  },
+  {
+    name: 'Elma Head',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+  },
+  {
+    name: 'Carey Barr',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+  },
+  {
+    name: 'Blackburn Dotson',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+  },
+  {
+    name: 'Sheree Anthony',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+  },
+];
+
+console.log(getUsersWithFriend(allUsers, 'Briana Decker')); 
+console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
+console.log(getUsersWithFriend(allUsers, 'Adrian Cross'));
